1ecb1cab1d83d511e9220ac02d63c76d5c358738
Talbot, Thomas <thomas.talbot@finastra.com>
2020-12-11 09:45:05 +0100

    fix(home): adding horizontal parallax on mouse move.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 // import of styles
 import './styles/index.scss'
+
+import { gsap } from "gsap"; 
+
+const middle = innerWidth / 2;
+
+window.addEventListener("mousemove", e => {
+  let pos = middle - e.pageX;
+  
+  gsap.to(".title", {x: -(0.10*pos), duration: 1});
+  gsap.to(".img1", {x: -(0.040*pos), duration: 1});
+  gsap.to(".img2", {x: -(0.025*pos), duration: 1});
+  gsap.to(".img3 ", {x: -(0.080*pos), duration: 1});
+  gsap.to(".crop-mark ", {x: -(0.075*pos), duration: 1});
+});
