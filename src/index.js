72e1cd4cb830504e3e0f79d8d3d8bf21ba8317c9
Talbot, Thomas <thomas.talbot@finastra.com>
2020-12-11 17:32:27 +0100

    fix(home): fix title animation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // import of styles
 import './styles/index.scss'
 
-import { gsap } from "gsap"; 
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
 
+gsap.registerPlugin(TextPlugin);
 const middle = innerWidth / 2;
+
+var tl = gsap.timeline({repeat: -1, repeatDelay: 1});
+
+tl.to(".title-emphasis", {y: -100, duration: 0.5}, 1);
+tl.set(".title-emphasis", {text: "change"}, 1.2);
+tl.set(".title-emphasis", {y: 80}, 1.2);
+tl.to(".title-emphasis", {y: 0, duration: 0.5}, 1.2);
+tl.to(".title-emphasis", {y: -100, duration: 0.5}, 4);
+tl.set(".title-emphasis", {text: "invent"}, 4.2);
+tl.set(".title-emphasis", {y: 80}, 4.2);
+tl.to(".title-emphasis", {y: 0, duration: 0.5}, 4.2);
+tl.to(".title-emphasis", {y: -100, duration: 0.5}, 7);
+tl.set(".title-emphasis", {text: "design"}, 7.2);
+tl.set(".title-emphasis", {y: 80}, 7.2);
+tl.to(".title-emphasis", {y: 0, duration: 0.5}, 7.2);
 
 window.addEventListener("mousemove", e => {
   let pos = middle - e.pageX;
